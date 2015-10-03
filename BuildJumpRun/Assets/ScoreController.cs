c0c89cc7d8e094b47f2b809ccd291741c5e78c85
Steven Taarland <steven.taarland@gmail.com>
2015-10-03 20:43:50 +0100

    score controller and ui

---
+++ BuildJumpRun/Assets/ScoreController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreController : MonoBehaviour 
+{
+	public GameObject scoreUI;
+	public GameObject livesUI;
+
+	public int startingLives = 10;
+
+	private int score = 0;
+	private int lives = 10;
+
+	//---------------------------------------------------------
+	//---------------------------------------------------------
+	void Start () 
+	{
+		lives = startingLives;
+	}
+	//---------------------------------------------------------
+	//---------------------------------------------------------
+	public void RemoveLives()
+	{
+		lives--;
+		livesUI.GetComponent<Text> ().text = "X " + lives.ToString();
+	}
+	//---------------------------------------------------------
+	//---------------------------------------------------------
+	public void AddScore(int in_score)
+	{
+		score += in_score;
+		scoreUI.GetComponent<Text> ().text = score.ToString();
+	}
+}
